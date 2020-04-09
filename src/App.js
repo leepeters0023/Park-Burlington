80c5fc06d10aa0451b35cb715d8544d580f8ed3a
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-04-09 15:38:37 -0400

    map can add park data on click

--- src/App.js
+++ src/App.js
@@ -3,27 +3,42 @@ import './App.css';
 import Header from './Header.js'
 import Maplet from './Maplet.js'
 
+
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state={
+    this.state = {
       spaces: [
-        {latitude: 44.4759, longitude: -73.2121},
-        {latitude: 44.4750, longitude: -73.2121},
-        {latitude: 44.4748, longitude: -73.2120},
-        {latitude: 44.4751, longitude: -73.2118},
-        {latitude: 44.4754, longitude: -73.2121}
-      ]
-    }
+        { latitude: 44.4759, longitude: -73.2121 },
+        { latitude: 44.4750, longitude: -73.2121 },
+        { latitude: 44.4748, longitude: -73.2120 },
+        { latitude: 44.4751, longitude: -73.2118 },
+        { latitude: 44.4754, longitude: -73.2121 }
+      ],
+
+      showParking: false
+    };
   }
-  render(){
-  return (
-    <div className="App">
-      <Header/>
-      <Maplet  
-      />
-    </div>
-  );
+
+  toggleParkingDisplay = () => {
+    if (this.state.showParking === true) {
+      this.setState({
+        showParking: false
+      })
+    } else {
+      this.setState({
+        showParking: true
+      })
+    }
+  };
+
+  render() {
+    return (
+      <div className="App"  >
+        <Header toggleParkingDisplay={this.toggleParkingDisplay} />
+        <Maplet showParking={this.state.showParking} />
+      </div>
+    );
   }
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,31 @@
+
 import React from 'react'
 
-export default function Header() {
-    return(
-        <div id='header'>
-            <img id="logo" src="logo-secondary.png"></img>
-            <img id="logo-text" src="logo-text.png"></img>
-        </div>
-    )
+
+
+
+export class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showParking: this.props.showParking,
+            toggleParkingDisplay: this.props.toggleParkingDisplay
+        }
+
+    }
+
+    render() {
+        return (
+            <div id='header'>
+                <img id="logo" src="logo-secondary.png" alt={'no logo'}></img>
+                <img id="logo-text" src="logo-text.png" alt={'no logo text'}></img>
+
+                {/* <div id='toggleSelector'> */}
+                <button onClick={this.props.toggleParkingDisplay} >Show / Hide Parking</button>
+                {/* </div> */}
+            </div>
+        )
+    }
 }
+
+export default Header
